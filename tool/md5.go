4a70dcaaebd297d8fe0a126a740c7acec7fca96e
hwholiday <heweiholiday@gmail.com>
2022-07-27 22:21:34 +0800

    add md5

---
+++ tool/md5.go
@@ -0,0 +1,17 @@
+package tool
+
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+func MD5(str string) string {
+	hash := md5.New()
+	hash.Write([]byte(str))
+	sum := hash.Sum(nil)
+	return fmt.Sprintf("%x\n", sum)
+}
+
+func MD5Salt(s string, salt string) string {
+	return MD5(s + salt)
+}
