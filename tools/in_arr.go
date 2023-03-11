de6bbc24c91f25cd096ca87da6b936dfb3db8d37
hwholiday <heweiholiday@gmail.com>
2023-03-12 07:10:16 +0800

    添加工具类

--- tools/in_arr.go
+++ tools/in_arr.go
@@ -9,6 +9,15 @@ func InIntArr(in int, arr []int) bool {
 	return false
 }
 
+func InInt64Arr(in int64, arr []int64) bool {
+	for _, v := range arr {
+		if in == v {
+			return true
+		}
+	}
+	return false
+}
+
 func InStrArr(in string, arr []string) bool {
 	for _, v := range arr {
 		if in == v {
