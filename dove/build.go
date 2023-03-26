c02f7f384fb09427102fcc8ea0635ed6d819f9ba
hwholiday <heweiholiday@gmail.com>
2023-03-27 00:07:44 +0800

    完善网络框架和例子

--- dove/build.go
+++ dove/build.go
@@ -12,6 +12,7 @@ type build struct {
 	dove         *api.Dove
 	doveMetadata *api.DoveMetadata
 	doveBody     *api.DoveBody
+	err          error
 }
 
 func NewDoveRes() *build {
@@ -19,11 +20,14 @@ func NewDoveRes() *build {
 }
 
 func (b *build) Result() ([]byte, error) {
-	if b.doveMetadata == nil {
+	if b.err != nil {
+		return nil, b.err
+	}
+	if b.doveMetadata.GetCrcId() == 0 {
 		return nil, errors.New("metadata is empty")
 	}
-	if b.doveBody == nil {
-		return nil, errors.New("body is empty")
+	if b.doveBody.GetCode() == 0 {
+		return nil, errors.New("body code is  ")
 	}
 	res := api.Dove{
 		Metadata: b.doveMetadata,
@@ -33,6 +37,9 @@ func (b *build) Result() ([]byte, error) {
 }
 
 func (b *build) MetadataSeq(seq string) *build {
+	if b.err != nil {
+		return b
+	}
 	if b.doveMetadata != nil {
 		b.doveMetadata.Seq = seq
 	} else {
@@ -45,41 +52,96 @@ func (b *build) MetadataSeq(seq string) *build {
 }
 
 func (b *build) Metadata(crcId, ackId uint64) *build {
-	b.doveMetadata = &api.DoveMetadata{
-		CrcId:     crcId,
-		AckId:     ackId,
-		Seq:       uuid.NewString(),
-		Timestamp: time.Now().UnixMilli(),
+	if b.err != nil {
+		return b
 	}
+	if b.doveMetadata != nil {
+		b.doveMetadata.CrcId = crcId
+		b.doveMetadata.AckId = ackId
+	} else {
+		b.doveMetadata = &api.DoveMetadata{
+			CrcId:     crcId,
+			AckId:     ackId,
+			Seq:       uuid.NewString(),
+			Timestamp: time.Now().UnixMilli(),
+		}
+	}
+
 	return b
 }
 
-func (b *build) BodyErr(code uint64, msg string) *build {
-	b.doveBody = &api.DoveBody{
-		Msg:  msg,
-		Code: code,
+func (b *build) BodyMsg(msg string) *build {
+	if b.err != nil {
+		return b
+	}
+	if b.doveBody != nil {
+		b.doveBody.Msg = msg
+	} else {
+		b.doveBody = &api.DoveBody{
+			Msg: msg,
+		}
 	}
 	return b
 }
 
 func (b *build) BodyOk(code ...uint64) *build {
+	if b.err != nil {
+		return b
+	}
 	var c = DefaultDoveBodyCodeOK
 	if len(code) > 1 {
 		c = code[0]
 	}
-	b.doveBody = &api.DoveBody{
-		Code: c,
+	return b.BodyCode(c)
+}
+
+func (b *build) BodyCode(code uint64) *build {
+	if b.err != nil {
+		return b
+	}
+	if b.doveBody != nil {
+		b.doveBody.Code = code
+	} else {
+		b.doveBody = &api.DoveBody{
+			Code: code,
+		}
 	}
 	return b
 }
-func (b *build) BodyOkWithData(data []byte, code ...uint64) *build {
-	var c = DefaultDoveBodyCodeOK
-	if len(code) > 1 {
-		c = code[0]
+func (b *build) BodyData(data []byte) *build {
+	if b.err != nil {
+		return b
 	}
-	b.doveBody = &api.DoveBody{
-		Code: c,
-		Data: data,
+	if b.doveBody != nil {
+		b.doveBody.Data = data
+	} else {
+		b.doveBody = &api.DoveBody{
+			Data: data,
+		}
+	}
+	return b
+}
+func (b *build) BodyPbData(data proto.Message) *build {
+	if b.err != nil {
+		return b
+	}
+	var byt []byte
+	byt, b.err = proto.Marshal(data)
+	if b.err != nil {
+		return b
+	}
+	return b.BodyData(byt)
+}
+func (b *build) BodyExpand(expand []byte) *build {
+	if b.err != nil {
+		return b
+	}
+	if b.doveBody != nil {
+		b.doveBody.Expand = expand
+	} else {
+		b.doveBody = &api.DoveBody{
+			Expand: expand,
+		}
 	}
 	return b
 }
